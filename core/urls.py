6229c8c3ed50a3d2803d78a4aaf649974d5d265d
Lorenzo Cabrera <lorenzo@girolabs.com>
2025-08-15 02:54:01 -0300

    feat: hide API root view in production

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, conf
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
 
 
 router = DefaultRouter()
 
+if not settings.DEBUG:
+    router.include_root_view = False
 
 urlpatterns = [
     path("", conf.include(router.urls)),
